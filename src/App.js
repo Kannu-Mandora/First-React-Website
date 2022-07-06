103b27397a188b381e62d7fb9e257d5ca4b31652
Kannu-Mandora <mandorakannu@gmail.com>
2022-07-06 16:21:01 +0530

    Making Navbar DarkMode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import Navbar from './components/Navbar'
 import Leftpanel from './components/Leftpanel'
 import Textfield from './components/Textfield'
 import './App.css';
+import React, { useState } from 'react'
+
+// ! Always pass the value of color,bg-color into the form of tailwindcss Framework otherwise, its won't work!
 
 function App() {
+ 
   return (
     <>
    <Navbar/>
